93b8e2b812a43302dce27e59552c37deea68aee2
Abdujabbor <shunchakiabdujabbor@gmail.com>
2025-10-20 23:09:23 +0500

    some changes!

--- erp_system/urls.py
+++ erp_system/urls.py
@@ -1,24 +1,20 @@
 # erp_system/urls.py
 from django.contrib import admin
 from django.urls import path, include
-<<<<<<< HEAD
+
 from django.conf.urls.static import static
 from . import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('student/', include('academics.urls')),
-
-    ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-=======
-
-urlpatterns = [
     path('admin/', admin.site.urls),
     
     path('auth/', include('accounts.urls')),
     
     path('', include('core.urls')), 
     
-    # path('api/', include('api.urls')), # Kelajakda API uchun
-]
->>>>>>> a29fb977a5829ec732df702013b2b1fca7ed585a
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+# path('api/', include('api.urls')), # Kelajakda API uchun
+#<<<<<<< HEAD>>>>>>> a29fb977a5829ec732df702013b2b1fca7ed585a
